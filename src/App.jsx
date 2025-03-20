f99b86448f18155f4fef7ab85c6a5af45eb7ae2a
ViestursK <95314459+ViestursK@users.noreply.github.com>
2025-03-20 21:57:46 +0200

    first prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// /src/App.js
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
 import "./styles/globals.css";
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(param) {
+  return (
+    <div>
+      <h1 style={{ margin: "0" }}>{param.prop}</h1>
+    </div>
+  );
+}
+
+export default Header;
